714576bfc1d8d0cc3dcb4d2c583ce81fb8cbeba0
ca-bee <66093924+ca-bee@users.noreply.github.com>
2020-06-14 14:54:58 -0400

    Update Patron.java

--- LibraryProject/src/com/company/Patron.java
+++ LibraryProject/src/com/company/Patron.java
@@ -1,8 +1,6 @@
 package com.company;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 
 
 /**
@@ -15,31 +13,24 @@ public class Patron {
     private String phoneNumber;
     private final int patronNumber;
     private static int last_patronNumber = 0;
-    private ArrayList<CheckedOutBook> checkedOut; //number of books checked out //figure out how to only store due dates and name of book
-    private final static int maxCheckOut = 3;//the maximum number of books that can be checked out at one time
-
+    private static final int maxCheckOut = 3;
+    private int indexOfCheckout = 0;
+    private Book[] checkedOut = new Book[2];
+    private LocalDate[] dueDates = new LocalDate[2];
 
     /**
      * Creates patrons of the library with their name, phone number, list of books checked out under their name. Each patron
      * has a unique ID.
      * @param name the patron's name
-     * @param phoneNumber the patron's phone #
-     * @param checkedOut a list of the books that have been checked out by this patron with corresponding due dates.
+     * @param phoneNumber the patron's phone number
      * **/
-    public Patron(String name, String phoneNumber, ArrayList<CheckedOutBook> checkedOut){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.checkedOut = checkedOut;
-        patronNumber = last_patronNumber;
-        last_patronNumber++;
-    }
-
     public Patron(String name, String phoneNumber){
         this.name = name;
         this.phoneNumber = phoneNumber;
         patronNumber = last_patronNumber;
         last_patronNumber++;
     }
+
 
     /**
      * Gets name.
@@ -59,6 +50,66 @@ public class Patron {
      * **/
     public int getPatronNumber(){return patronNumber;}
 
-    
+
+    /**
+     * Adds a certain book and its due date to the list of books that the patron has checked out. It also checks to see
+     * if the patron has taken out the maximum number of books which is three. It also makes sure that the status is
+     * checked out.
+     * @param b the book that the patron wants taken out
+     * @return true if the book was successfully checked out and false if the book is not successfully checked out.
+     * **/
+    public boolean checkOutPatron(Book b){
+        if((indexOfCheckout < maxCheckOut) && (b.getCheckOut()==false)){//patron hasn't taken out max number of books
+            checkedOut[indexOfCheckout] = b; //adds certain book to list of checkout
+            LocalDate today = LocalDate.now();
+            LocalDate due = today.plusWeeks(3);
+            dueDates[indexOfCheckout] = due;
+            b.changeCheckOut();//book now has status of checked out
+            indexOfCheckout++;
+            return true;
+        }
+        else {return false;}
+    }
+
+    /**
+     *Removes the book from the list of books that the patron has checked out. Makes sure that the status is checked in.
+     * @param b the book that is being checked in.
+     * @return true if the book has been successfully checked in and false if the book was not successfully checked in.
+     * **/
+    public boolean checkInPatron(Book b){
+        for(int i = 0; i < checkedOut.length; i++){
+            if((b.getBookNumber())==(checkedOut[i].getBookNumber()) && (b.getCheckOut())) {
+                /*Checks if IDs match and if that book can be checked in before checking it in*/
+                checkedOut[i] = checkedOut[i + 1];//shift everything over
+                dueDates[i] = dueDates[i + 1];
+                b.changeCheckOut();//book now has status of checked in
+                indexOfCheckout--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*/**
+     * Calculates the current amount of fees that needs to be paid by the patron. It takes the current
+     * day and checks to see if it comes after the due date of the books borrowed. If it is after, it
+     * adds the fee which is 10 cents per day for each late book and calculates the total fee.
+     * @param today the current day used to check how much fees are currently owed.
+     * @return the total amount of fees owed by the patron in dollars, which will be zero if no fees are owed.
+     * **/
+    /*public double calculateFees(LocalDate today){
+
+        LocalDate[] dueDateList = new LocalDate[checkedOut.size()];
+        double fee = 0.00;
+        for(int i = 0; i < dueDateList.length; i++){//goes through list of due dates and find book that is being checked in
+            dueDateList[i] = (checkedOut.get(i)).getDueDate();
+            if(today.isAfter(dueDateList[i])){//check if checkInDate is after dueDate
+                double noOfDaysBetween = ChronoUnit.DAYS.between(dueDateList[i], today);
+                fee+= (noOfDaysBetween)*(0.10);//multiply by 0.10 and add to sum of total fees
+            }
+        }
+        return fee;//total sum of fees
+    }*/
 
 }
+
